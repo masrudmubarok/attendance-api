18bf42c61b5e5d1ceede45a4110186627c9aa83e
masrudmubarok <masrud.mubarok21@gmail.com>
2025-02-27 03:19:14 +0700

    nitial local setup

---
+++ config/index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+module.exports = {
+  db: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+  redis: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+  },
+  es: {
+    node: process.env.ELASTICSEARCH_URL,
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
+  port: process.env.PORT || 3000,
+};
