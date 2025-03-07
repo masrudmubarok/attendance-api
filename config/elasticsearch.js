f8e1fa5bd0924307ce454b9f22581c6c4cb7950b
masrudmubarok <masrud.mubarok21@gmail.com>
2025-03-08 04:24:52 +0700

    Bugfix elasticsearch & redis connection

--- config/elasticsearch.js
+++ config/elasticsearch.js
@@ -3,14 +3,38 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const ELASTICSEARCH_HOST = process.env.ELASTICSEARCH_HOST ?? 'localhost';
-const ELASTICSEARCH_PORT = process.env.ELASTICSEARCH_PORT ?? 9200;
+const ELASTIC_CLOUD = process.env.ELASTIC_CLOUD;
 
-const elasticUrl = `http://${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT}`;
+let elasticsearch;
 
-const elasticsearch = new Client({
-  node: elasticUrl,
-});
+if (ELASTIC_CLOUD) {
+  const ELASTIC_CLOUD_ID = process.env.ELASTIC_CLOUD_ID;
+  const ELASTIC_USERNAME = process.env.ELASTIC_USERNAME;
+  const ELASTIC_PASSWORD = process.env.ELASTIC_PASSWORD;
+
+  if (!ELASTIC_CLOUD_ID || !ELASTIC_USERNAME || !ELASTIC_PASSWORD) {
+    console.error(
+      'Variabel lingkungan ELASTIC_CLOUD_ID, ELASTIC_USERNAME, atau ELASTIC_PASSWORD tidak ditemukan.'
+    );
+    process.exit(1);
+  }
+
+  elasticsearch = new Client({
+    cloud: { id: ELASTIC_CLOUD_ID },
+    auth: {
+      username: ELASTIC_USERNAME,
+      password: ELASTIC_PASSWORD,
+    },
+  });
+} else {
+  // Konfigurasi Elasticsearch Lokal
+  const ELASTICSEARCH_HOST = process.env.ELASTICSEARCH_HOST || 'localhost';
+  const ELASTICSEARCH_PORT = process.env.ELASTICSEARCH_PORT || 9200;
+
+  elasticsearch = new Client({
+    node: `${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT}`,
+  });
+}
 
 async function checkElasticsearchConnection() {
   try {
--- config/redis.js
+++ config/redis.js
@@ -1,8 +1,21 @@
 import Redis from "ioredis";
 
-const redis = new Redis({
-  host: process.env.REDIS_HOST || "127.0.0.1",
-  port: process.env.REDIS_PORT || 6379,
+const redisHost = process.env.REDIS_HOST;
+const redisPassword = process.env.REDIS_PASSWORD;
+const redisPort = process.env.REDIS_PORT;
+
+if (!redisPassword || !redisHost|| !redisPort) {
+  console.error("Environment redis not complete.");
+}
+
+const redis = new Redis(`rediss://:${redisPassword}@${redisHost}:${redisPort}`);
+
+redis.on("connect", () => {
+  console.log("Connected to Redis");
+});
+
+redis.on("error", (err) => {
+  console.error("[ioredis] Error:", err);
 });
 
 export default redis;
